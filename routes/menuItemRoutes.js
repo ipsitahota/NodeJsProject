ece882533503217216ce36a294aa2f942fbc30ea
ipsita hota <ipsitahota48@gmail.com>
2025-01-27 11:46:21 +0530

    tiny changes 2

--- routes/menuItemRoutes.js
+++ routes/menuItemRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const MenuItem = require('../models/MenuItem' )
+const MenuItem = require('./../models/MenuItem.js' )
 
 router.post('/',async (req,res) =>{
     try{

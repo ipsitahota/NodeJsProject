f472df627e3f286ee240e508e7bea720dd0f1245
ipsita hota <ipsitahota48@gmail.com>
2025-01-27 11:42:50 +0530

    tiny changes

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,7 +1,7 @@
 //express router
 const express = require('express');
 const router = express.Router();
-const Person = require('./../models/Person' )
+const Person = require('../models/Person' )
 
   router.get('/',async (req,res)=>{
     try{
